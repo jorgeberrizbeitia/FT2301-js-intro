5ffad14d00c1362580c92f1de145a0ebdc0747cb
jorgeberrizbeitia <jorgeberrizbeitia@gmail.com>
2023-01-17 12:08:05 +0100

    strings done

--- script.js
+++ script.js
@@ -30,7 +30,7 @@ anotherHobby = "Bailar";
 
 console.log(anotherHobby)
 
-// let hobby;
+// let hobby; // no puede crear variables con el mismo nombre (let o const)
 
 // cosas que no puedo o no debo hacer en JS
 
@@ -92,3 +92,77 @@ console.log( typeof NaN )
 
 let variable8 = []; // los arrays no son su propio de data. comparte el mismo que los objetos.
 console.log( typeof variable8 )
+
+
+
+// STRINGS
+
+
+let fruit1 = "banana";
+let fruit2 = "pera";
+let fruit3 = "fresa";
+
+
+let recipe = "Mi receta de Smoothie lleva: " + fruit1 + " y tambien " + fruit2
+console.log(recipe)
+
+// en vez de contatenar, tenemos acceso a algo que se llama interpolacion de strings
+// para esto solo se puede si usamos backticks ``
+let recipeTotal = `Mi receta de Smoothie lleva: ${fruit1}, ${fruit2}, y tambien ${fruit3}`
+console.log(recipeTotal)
+
+
+// posiciones de caracteres (index) en strings
+
+let simpleWord = "Hola";
+//                ||||
+//                0123
+
+console.log(simpleWord)
+console.log(simpleWord[0])
+
+console.log(simpleWord[2])
+
+// length (propiedades) cantidad de caracteres
+
+console.log( simpleWord.length )
+
+// como hago console.log del ultimo caracter?
+
+// let lastPosition = simpleWord.length - 1
+console.log( simpleWord[simpleWord.length - 1] ) // ultimo caracter (sin importar largo de string)
+
+
+
+// Algunos metodos de strings
+
+
+// indexOf
+
+let song = "bananaramawakawakaehehsamiramira";
+
+console.log( song.indexOf("w") ) // busca posicion de caracter. -1 es que no existe
+
+
+// console.log( song.slice(10, 14) )
+let newStr = song.slice(10, 14)
+console.log(newStr.toUpperCase())
+
+console.log( song.toUpperCase() )
+
+
+console.log( song.slice(16, 24).toUpperCase() ) // lo mismo que arriba en una sola linea
+
+
+let newWord = "bob"
+
+
+
+let firstLetter = newWord[0].toUpperCase()
+let restofWord = newWord.slice(1)
+
+let capitalizedWord = firstLetter + restofWord
+console.log(capitalizedWord)
+
+
+console.log( newWord[0].toUpperCase() + newWord.slice(1, newWord.length) )
